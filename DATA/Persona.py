946a36d5f4e8fc6a5638f921b8119d10a3d187e3
mcolmenares <mcolmenares@unal.edu.co>
2021-04-22 10:37:26 -0500

    Agregué los gets and sets y el metodo toString

--- DATA/Persona.py
+++ DATA/Persona.py
@@ -9,15 +9,55 @@ class Persona:
         self.__foto = foto
         self.__telefono = telefono
 
-    def getId(self):
-        return self.__id
-    def setId(self,ID):
-        self.__id = ID
-        
-    def getFoto(self):
-        return self.__foto
-    def setFoto(self, foto):
-        self.__foto = foto
+        # Getts and Setts
+
+        def getApellido(self):
+            return self.__Apellido
+
+        def setApellido(self, apellido):
+            self.__Apellido = apellido
+
+        def getCorreo(self):
+            return self.__Correo
+
+        def setCorreo(self, correo):
+            self.__Correo = correo
+
+        def getFoto(self):
+            return self.__Foto
+
+        def setFoto(self, foto):
+            self.__Foto = foto
+
+        def getClave(self):
+            return self.__Clave
+
+        def setClave(self, clave):
+            self.__Clave = clave
+
+        def getIdentificacion(self):
+            return self.__Identificacion
+
+        def setIdentificacion(self, ident):
+            self.__Identificacion = ident
+
+        def getID(self):
+            return self.__ID
+
+        def setID(self, Id):
+            self.__ID = Id
+
+        def getNombre(self):
+            return self.__Nombre
+
+        def setNombre(self, nombre):
+            self.__Nombre = nombre
+
+        def getTelefono(self):
+            return self.__Telefono
+
+        def setTelefono(self, telefono):
+            self.__Telefono = telefono
 
     def __str__(self):
 
